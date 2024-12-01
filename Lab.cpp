0cea02995bee513b5249fb65ad16ebf9bf7dcc46
Eduardoz27 <ezarate6@student.cccd.edu>
2024-12-01 00:36:57 -0800

    Testing

---
+++ Lab.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Includes the input/output library
+
+int main() {
+    // Print the message to the console
+    std::cout << "W14 Lab Assignment" << std::endl;
+    return 0; // Indicate successful program execution
+}
